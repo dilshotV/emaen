a04f6f9d150811bb56a52efed5fb62af11715664
dilshotV <dilshotvargas@gmail.com>
2026-06-11 10:18:19 -0600

    Add files via upload

---
+++ Sem04Practica/src/sem04practica/Pagina.java
@@ -0,0 +1,18 @@
+
+package sem04practica;
+
+public class Pagina {
+    private String nombre;
+
+    public Pagina() {
+        this.nombre = "";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ Sem04Practica/src/sem04practica/Principal.java
@@ -0,0 +1,11 @@
+
+package sem04practica;
+
+public class Principal {
+
+    public static void main(String[] args) {
+       Menu m = new Menu();
+        m.menu();
+    }
+    
+}
